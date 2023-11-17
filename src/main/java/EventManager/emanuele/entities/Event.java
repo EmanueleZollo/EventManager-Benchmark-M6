95608d05d9f26dd6a168abc0de38dae6752434f8
Emanuele Zollo - NjS Studio <ez.studionjs@gmail.com>
2023-11-17 14:36:55 +0100

    Event entity

--- src/main/java/EventManager/emanuele/entities/Event.java
+++ src/main/java/EventManager/emanuele/entities/Event.java
@@ -1,4 +1,27 @@
 package EventManager.emanuele.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "eventi")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Event {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String title;
+    private String description;
+    private LocalDate eventDay;
+    private String location;
+    private int audienceCapacity;
 }
